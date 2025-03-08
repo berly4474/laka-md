bbbd6903ffcee504f03af2d6ffe7eb519015e1b1
berly4474 <jeantyberlly51@gmail.com>
2025-03-08 14:43:23 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=4wZRnRQC#fuN78D-qeHaFhBdwqIpEiJ89cvAgeG2yaO6ZS3ooivg",
 PREFIX: process.env.PREFIX || '.' ,
-MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
-AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
-AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "false" : process.env.AUTO_REPLY,
+MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
+AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
+AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "true" : process.env.AUTO_REPLY,
 AUTO_STICKER: process.env.AUTO_STICKER === undefined ? "false" : process.env.AUTO_STICKER,
 WELCOME_GOODBAY: process.env.WELCOME_GOODBAY === undefined ? "true" : process.env.WELCOME_GOODBAY,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "true" : process.env.AUTO_READ_STATUS,
